964714292e7b434fb41205559fdc77f1c13a6eff
Will <william.campbell44@gmail.com>
2024-01-03 22:14:17 +0000

    Convert rustls_config return type to Result

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::BufReader, fs::File, time::Duration};
+use std::{io::BufReader, fs::File, time::Duration, error::Error};
 
 use actix_web::{HttpServer, App};
 use rustls::{ServerConfig, Certificate, PrivateKey};
@@ -19,7 +19,7 @@ async fn main() -> std::io::Result<()> {
             .service(endpoints::get::public_router)
             .service(endpoints::get::private_router)
     })
-    .bind_rustls_021(&config.bind_ip, load_rustls_config())?
+    .bind_rustls_021(&config.bind_ip, load_rustls_config().unwrap())?
     .keep_alive(Duration::from_secs(75))
     .run()
     .await
@@ -27,32 +27,24 @@ async fn main() -> std::io::Result<()> {
 
 // taken from SO
 #[inline]
-fn load_rustls_config() -> ServerConfig {
+fn load_rustls_config() -> Result<ServerConfig, Box<dyn Error>> {
     let config = ServerConfig::builder()
         .with_safe_defaults()
         .with_no_client_auth();
 
     // load TLS key/cert files
-    let cert_file = &mut BufReader::new(File::open("cert.pem").unwrap());
-    let key_file = &mut BufReader::new(File::open("key.pem").unwrap());
+    let cert_file = &mut BufReader::new(File::open("cert.pem")?);
+    let key_file = &mut BufReader::new(File::open("key.pem")?);
 
     // convert files to key/cert objects
-    let cert_chain = certs(cert_file)
-        .unwrap()
+    let cert_chain = certs(cert_file)?
         .into_iter()
         .map(Certificate)
         .collect();
-    let mut keys: Vec<PrivateKey> = pkcs8_private_keys(key_file)
-        .unwrap()
+    let mut keys: Vec<PrivateKey> = pkcs8_private_keys(key_file)?
         .into_iter()
         .map(PrivateKey)
         .collect();
 
-    // exit if no keys could be parsed
-    if keys.is_empty() {
-        eprintln!("Could not locate PKCS 8 private keys.");
-        std::process::exit(1);
-    }
-
-    config.with_single_cert(cert_chain, keys.remove(0)).unwrap()
+    Ok(config.with_single_cert(cert_chain, keys.remove(0))?)
 }
